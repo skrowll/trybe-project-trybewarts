76772f1ef50720beeeef63cffda895dfa7008686
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-11 17:55:04 -0300

    Requisito 3 iniciado

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+//DOM Selection
+let enterButton = document.querySelector('#enter-button');
+let emailInput = document.querySelector('#email');
+let passwordInput = document.querySelector('#password');
+
+//Event Listener
+enterButton.addEventListener('click', Login);
+
+//Functions
+function Login() {
+    if (emailInput.value === "tryber@teste.com" && passwordInput.value === "123456") {
+        window.alert("Olá, Tryber!");
+    } else {
+        window.alert("Email ou senha inválidos.");
+    }
+};
+
+};
